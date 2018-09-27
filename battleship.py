39831473e4896fd1bb11cf119697f743701ede68
Ryan Brasseaux <43624863+BroCode23@users.noreply.github.com>
2018-09-27 16:17:03 -0500

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -14,21 +14,27 @@ print("battleship (5 pegs)")
 orient = input("what orientation would you like your ship? (v/h)")
 orient = orient[0].lower()
 if orient == "v":
-  while not 0 <= btm <= (10 - boat):
-    btm = int(input("Space from bottom?"))
-    btm = 10 - (boat + (btm % 10))
-  while not 0 <= lef <= 9:
-    lef = int(input("Space from left?"))
-    lef = lef % 10
+	while not 0 <= btm <= (10 - boat):
+    	btm = int(input("Space from bottom?"))
+        btm = 10 - (boat + (btm % 10))
+   	while not 0 <= lef <= 9:
+      	lef = int(input("Space from left?"))
+      	lef = lef % 10
 else:
-  while not 0 <= btm <= 9:
-    btm = int(input("Space from bottom?"))
-    btm = 10 - (1 + (btm % 10))
-  while not 0 <= lef <= (10 - boat):
-    lef = int(input("Space from left?"))
-    lef = lef % 10
+ 	while not 0 <= btm <= 9:
+      	btm = int(input("Space from bottom?"))
+       	btm = 10 - (1 + (btm % 10))
+   	while not 0 <= lef <= (10 - boat):
+      	lef = int(input("Space from left?"))
+       	lef = lef % 10
 if orient == "h":
-  grid[
+   	grid[(btm * 10 - 1 + lef):(btm * 10 - 1 + boat - 1 + lef)] = "X"
+elif orient == "v":
+	x = btm * 10 - 1 + lef + 10 * boat
+	for y in boat
+    	grid[x] = "X"
+
+	
 
   
 
